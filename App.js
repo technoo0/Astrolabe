03fff391f6ca2adc0611d15472d0fb06690bfc86
MahmoudSheemy127 <mahmoudalshemy.3@gmail.com>
2021-09-29 22:25:45 +0200

    Navigation Complete

--- App.js
+++ App.js
@@ -1,5 +1,20 @@
 import React from "react";
 import ARScreen from "./src/screens/ARScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./src/components/Home";
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  return <ARScreen />;
+  return(
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }} initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ARScreen" component={ARScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  ) 
 }
---
+++ src/components/Home.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import Svg, { Path } from "react-native-svg";
+
+const fetchFonts = () => {
+  Font.loadAsync({
+    "ropa-sans": require("../../fonts/RopaSans-Regular.ttf"),
+  });
+};
+
+export default function Home({navigation}) {
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
+  return (
+    <View style={styles.screen}>
+      <Svg
+        height="13%"
+        width="100%"
+        viewBox="0 0 1440 320"
+        style={{ position: "absolute" }}
+      >
+        <Path
+          fill="#F2EFEA"
+          d="M0,256L80,266.7C160,277,320,299,480,277.3C640,256,800,192,960,154.7C1120,117,1280,107,1360,101.3L1440,96L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+        />
+      </Svg>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Astrolabe</Text>
+      </View>
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.description}>
+          Earth Orbit is Full With Debris and Junk
+        </Text>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={{ fontSize: 15 }} onPress={() => navigation.navigate('ARScreen')}>See What it Looks Like!</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    alignItems: "center",
+    flex: 1,
+    paddingTop: 40,
+    backgroundColor: "#1E1E24",
+  },
+  titleContainer: {
+    marginTop: "25%",
+  },
+  title: {
+    fontSize: 65,
+    color: "white",
+    fontFamily: "ropa-sans",
+  },
+  descriptionContainer: {
+    marginTop: "10%",
+  },
+  description: {
+    fontSize: 18,
+    color: "white",
+  },
+  button: {
+    backgroundColor: "#F2EFEA",
+    width: 201,
+    height: 69,
+    borderRadius: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "50%",
+  },
+});
