bfc81fa43df5dfb7ab47f6080c69cca6ab17a839
MahmoudSheemy127 <mahmoudalshemy.3@gmail.com>
2021-09-30 22:15:38 +0200

    Navigation2

--- src/screens/ARScreen.js
+++ src/screens/ARScreen.js
@@ -6,9 +6,10 @@ import { Camera } from "expo-camera";
 
 let myControls = null;
 
-export default function ARScreen({}) {
+export default function ARScreen({navigation}) {
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const [slider,showSlider] = useState(false);
   const myCamera = useRef(null);
   useEffect(() => {
     (async () => {
@@ -30,11 +31,17 @@ export default function ARScreen({}) {
         <View style={styles.grid}>
           <ThreeDView />
         </View>
+        {
+        slider &&  
+        <View style={styles.timeStampContainer}>
+            <Text>Time Slider</Text>
+        </View>
+        }
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.backButton}>
+          <TouchableOpacity onPress={() => {navigation.goBack()}} style={styles.backButton}>
             <Image source={require("../../assets/back-arrow.png")} />
           </TouchableOpacity>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => {showSlider(!slider)}}>
             <Image source={require("../../assets/time-icon.png")} />
           </TouchableOpacity>
         </View>
@@ -56,4 +63,9 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
   },
+  timeStampContainer:{
+    width:"100%",
+    height:50,
+    backgroundColor:'white'
+  }
 });
